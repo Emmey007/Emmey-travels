fd49ed66dad12e8f99b716f5cfee97ccfb81a890
Emmanuel <emmeyemmanuel558@gmail.com>
2026-08-11 02:33:45 -0700

    optimize MongoDB connection for serverless

--- config/db.js
+++ config/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 const envObj = require('./env')
+
+let isConnected = false
+
 const connectDB = async () => {
+    if (isConnected) return
+
     try {
-        const db = await mongoose.connect(envObj.mongodbUrl)
-        if (db) console.log('EMMEY Travels Database Connected Successfully')
+        const db = await mongoose.connect(envObj.mongodbUrl, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        })
+        isConnected = true
+        console.log('EMMEY Travels Database Connected Successfully')
     } catch (error) {
         console.log(error.message)
     }
 }
+
 module.exports = connectDB
